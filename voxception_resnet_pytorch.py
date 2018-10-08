a3d0ef401c3dc712e7985aece0634863fa48b3f6
varun khare <varunkhare1234@gmail.com>
2018-10-08 13:28:00 +0530

    Update voxception_resnet_pytorch.py

--- voxception_resnet_pytorch.py
+++ voxception_resnet_pytorch.py
@@ -10,7 +10,6 @@ import pandas as pd
 import torchvision.models as models_res
 import matplotlib.pyplot as plt
 from skimage import io, transform
-
     
 def weights_init(m):
     ##later call net = voxception(); net.apply(weights_init) to recursively set init
@@ -19,7 +18,7 @@ def weights_init(m):
         nn.init.orthogonal_(m.bias.data, gain = torch.sqrt(2))
     elif isinstance(m, nn.BatchNorm3d):
         nn.init.constant_(m.weight, 1)
-        nn.init.constant_(m.bias, 0
+        nn.init.constant_(m.bias, 0)
 #   explicitly set the init of these two layers to gain=1 later
 #         nn.init.orthogonal_(net.input_conv.weight)
 #         nn.init.orthogonal_(net.fc[-1].weight)
@@ -28,46 +27,46 @@ class Voxception_Downsample_Layer(nn.Module):
     def __init__(self,numChannels,batch_norm=False,elu=False):
         super(Voxception_Downsample_Layer, self).__init__()
         if (not elu) and (not batch_norm):
-            self.branch0 = nn.Conv3d(numChannels, numChannels/2, kernel_size=3, stride=2)
-            self.branch1 = nn.Conv3d(numChannels, numChannels/2, kernel_size=1, stride=2)
+            self.branch0 = nn.Conv3d(numChannels, int(numChannels/2), kernel_size=3, stride=2,padding=1)
+            self.branch1 = nn.Conv3d(numChannels, int(numChannels/2), kernel_size=1, stride=2)
             self.branch2 = nn.Sequential(
-                nn.Conv3d(numChannels, numChannels/2, kernel_size=3, stride=1,padding=1),
+                nn.Conv3d(numChannels, int(numChannels/2), kernel_size=3, stride=1,padding=1),
                 nn.AvgPool3d(3, stride=2, padding=1)
             )
 
             self.branch3 = nn.Sequential(
-                nn.Conv3d(numChannels, numChannels/2, kernel_size=3, stride=1,padding=1),
+                nn.Conv3d(numChannels, int(numChannels/2), kernel_size=3, stride=1,padding=1),
                 nn.MaxPool3d(3, stride=2, padding=1)
             )
         elif (not elu) and batch_norm:
-            self.branch0 = nn.Sequential(nn.Conv3d(numChannels, numChannels/2, kernel_size=3, stride=2),
-                                         nn.BatchNorm3d(numChannels/2,eps=0.001))
-            self.branch1 = nn.Sequential(nn.Conv3d(numChannels, numChannels/2, kernel_size=1, stride=2),
-                                         nn.BatchNorm3d(numChannels/2,eps=0.001))
+            self.branch0 = nn.Sequential(nn.Conv3d(numChannels, int(numChannels/2), kernel_size=3, stride=2,padding=1),
+                                         nn.BatchNorm3d(int(numChannels/2),eps=0.001))
+            self.branch1 = nn.Sequential(nn.Conv3d(numChannels, int(numChannels/2), kernel_size=1, stride=2),
+                                         nn.BatchNorm3d(int(numChannels/2),eps=0.001))
             self.branch2 = nn.Sequential(
-                nn.Conv3d(numChannels, numChannels/2, kernel_size=3, stride=1,padding=1),
+                nn.Conv3d(numChannels, int(numChannels/2), kernel_size=3, stride=1,padding=1),
                 nn.AvgPool3d(3, stride=2, padding=1),
-                nn.BatchNorm3d(numChannels/2,eps=0.001)
+                nn.BatchNorm3d(int(numChannels/2),eps=0.001)
             )
 
             self.branch3 = nn.Sequential(
-                nn.Conv3d(numChannels, numChannels/2, kernel_size=3, stride=1,padding=1),
+                nn.Conv3d(numChannels, int(numChannels/2), kernel_size=3, stride=1,padding=1),
                 nn.MaxPool3d(3, stride=2, padding=1),
-                nn.BatchNorm3d(numChannels/2,eps=0.001)
+                nn.BatchNorm3d(int(numChannels/2),eps=0.001)
             )
         else:
-            self.branch0 = BasicConv3d(numChannels, numChannels/2, kernel_size=3, stride=2)
-            self.branch1 = BasicConv3d(numChannels, numChannels/2, kernel_size=1, stride=2)
+            self.branch0 = BasicConv3d(numChannels, int(numChannels/2), kernel_size=3, stride=2,padding=1)
+            self.branch1 = BasicConv3d(numChannels, int(numChannels/2), kernel_size=1, stride=2)
             self.branch2 = nn.Sequential(
-                nn.Conv3d(numChannels, numChannels/2, kernel_size=3, stride=1,padding-1),
+                nn.Conv3d(numChannels, int(numChannels/2), kernel_size=3, stride=1,padding=1),
                 nn.AvgPool3d(3, stride=2, padding=1),
-                nn.BatchNorm3d(numChannels/2,eps=0.001),nn.ELU()
+                nn.BatchNorm3d(int(numChannels/2),eps=0.001),nn.ELU()
             )
 
             self.branch3 = nn.Sequential(
-                nn.Conv3d(numChannels, numChannels/2, kernel_size=3, stride=1,padding=1),
+                nn.Conv3d(numChannels, int(numChannels/2), kernel_size=3, stride=1,padding=1),
                 nn.MaxPool3d(3, stride=2, padding=1),
-                nn.BatchNorm3d(numChannels/2,eps=0.001),nn.ELU()
+                nn.BatchNorm3d(int(numChannels/2),eps=0.001),nn.ELU()
             )
 
 
@@ -85,16 +84,15 @@ class Voxception_Resnet_Layer(nn.Module):
 
     def __init__(self,numChannels):
         super(Voxception_Resnet_Layer, self).__init__()
-
         self.branch1 = nn.Sequential(
-            BasicConv3d(numChannels, numChannels/4, kernel_size=3, stride=1),
-            nn.Conv3d(numChannels/2, numChannels/2, kernel_size=3, stride=1)
+            BasicConv3d(numChannels, int(numChannels/4), kernel_size=3, stride=1,padding=1),
+            nn.Conv3d(int(numChannels/4), int(numChannels/2), kernel_size=3, stride=1,padding=1)
         )
 
         self.branch2 = nn.Sequential(
-            BasicConv3d(numChannels, numChannels/4, kernel_size=1, stride=1),
-            BasicConv3d(numChannels/4, numChannels/4, kernel_size=3, stride=1),
-            nn.Conv3d(numChannels/4, numChannels/2, kernel_size=1, stride=1)
+            BasicConv3d(numChannels, int(numChannels/4), kernel_size=1, stride=1),
+            BasicConv3d(int(numChannels/4), int(numChannels/4), kernel_size=3, stride=1,padding=1),
+            nn.Conv3d(int(numChannels/4), int(numChannels/2), kernel_size=1, stride=1)
         )
 
     def forward(self, x):
@@ -128,25 +126,24 @@ class Resdrop(nn.Module):
         self.nn_module = drop_layer
         self.death_rate = p
 
+    
     def forward(self, x):
         residual = x
         sample=0
-        if not self.training:
-            sample = 1
+        if not self.training or torch.rand(1)[0] >= self.death_rate:
             residual = self.nn_module(residual)
-        else:
-            sample = Bernoulli(torch.tensor([1-self.deathRate])).sample()
-            residual /= (1. - self.death_rate)
-        
-        return x + sample*residual
+            if self.training:
+                residual /= (1. - self.death_rate)
+            x = x + residual
+        return x
 
 
 
 class VoxceptionNet(nn.Module):
-    def __init__(self):
+    def __init__(self,n_classes):
         super(VoxceptionNet, self).__init__() 
         
-        self.input_conv = nn.Conv3d(64, 32, kernel_size=3, stride=1)
+        self.input_conv = nn.Conv3d(1, 32, kernel_size=3, stride=1)
         self.b1 = nn.Sequential(Resdrop(nn.Sequential(nn.BatchNorm3d(32,eps=0.001),nn.ELU(),Voxception_Resnet_Layer(numChannels=32)),0.95),
                                 Resdrop(nn.Sequential(nn.BatchNorm3d(32,eps=0.001),nn.ELU(),Voxception_Resnet_Layer(numChannels=32)),0.90),
                                 Resdrop(nn.Sequential(nn.BatchNorm3d(32,eps=0.001),nn.ELU(),Voxception_Resnet_Layer(numChannels=32)),0.80),
@@ -171,11 +168,9 @@ class VoxceptionNet(nn.Module):
                                 nn.BatchNorm3d(256,eps=0.001),nn.ELU(),
                                 Voxception_Downsample_Layer(numChannels=256,elu=True))
 
-        self.b5 = nn.Sequential(Resdrop(nn.Sequential(nn.Conv3d(512,512,3,1),nn.BatchNorm3d(512,eps=0.001)),0.5),nn.ELU())
+        self.b5 = nn.Sequential(Resdrop(nn.Sequential(nn.Conv3d(512,512,3,1,1),nn.BatchNorm3d(512,eps=0.001)),0.5),nn.ELU())
         #add view numFeatures
-        self.fc = nn.Sequential(nn.Linear(,512),nn.BatchNorm1d(512),nn.ELU(),nn.Linear(512,n_classes))
-        
-        self.modules = [self.input_conv,self.b1,self.b2,self.b3,self.b3,self.b4,self.b5,self.fc]
+        self.fc = nn.Sequential(nn.Linear(512,512),nn.BatchNorm1d(512),nn.ELU(),nn.Linear(512,n_classes),nn.Softmax(1))
 
 
                               
@@ -185,12 +180,9 @@ class VoxceptionNet(nn.Module):
         x = self.b2(x)
         x = self.b3(x)
         x = self.b4(x)
-        #todo add torch.view here
-        out = F.max_pool3d()
-        out = self.b5(out)
+        x = self.b5(x)
+        out = F.max_pool3d(x, kernel_size=x.size()[2:])
+        out = out.view(-1,512)
         output = self.fc(out)
         return output
-        
-        
-        
         
